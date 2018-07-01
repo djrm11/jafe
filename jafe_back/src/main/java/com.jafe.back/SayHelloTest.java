45882dde6b4199851459c7c493a2fba5dda026b4
yangzefang <yangzefang@lvmama.com>
2018-07-01 21:52:20 +0800

    frist commit

---
+++ jafe_back/src/main/java/com.jafe.back/SayHelloTest.java
@@ -0,0 +1,11 @@
+package com.jafe.back;
+
+import com.jafe.comm.SayHello;
+
+public class SayHelloTest{
+
+    public static void main(String[] args){
+        System.out.println(new SayHello().sayHello("jafe")) ;
+
+    }
+}
---
+++ jafe_comm/src/main/java/com/jafe/comm/SayHello.java
@@ -0,0 +1,8 @@
+package com.jafe.comm;
+
+public class SayHello {
+
+    public String sayHello(String name){
+        return "hello " + name + "!";
+    }
+}
